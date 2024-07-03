3e2a7f353aeb5bdf93d1b3920d363dbb1c541327
Joseph E. Santos <101375998+Josephegs@users.noreply.github.com>
2024-07-03 14:03:43 +0000

    Endpoint Create [POST] - add item

--- index.js
+++ index.js
@@ -13,9 +13,25 @@ app.get('/oi', function (req, res){
 // LISTA DE PERSONAGENS
 const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
 
-// Read All - (endpoint)[GET] /item
-
+// Read All - (endpoint) [GET] /item
 app.get('/item', function (req, res) {
+    // Pega a lista e retorna como resposta do /item (no HTTP)
     res.send(lista)
 })
+
+// Sinalizar p/ o Express qual o formato da requisição usar (JSON)
+app.use(express.json())
+
+// Create - [POST] /item
+app.post('/item', function (req, res) {
+    // Obtem o nome da requisição
+    const item = req.body.nome
+
+    // Inserir o item no final da lista
+    lista.push(item)
+
+    //Envia uma mensagem de sucesso
+    res.send ('Intem criado com sucesso!')
+})
+
 app.listen(3000)
